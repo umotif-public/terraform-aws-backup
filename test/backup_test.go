f7b18bfe53f9c46a15b7be2e4c8237451abfd84f
Abdul Wahid <abdul.wahid@umotif.com>
2020-12-15 11:58:50 +0000

    golint

--- test/backup_test.go
+++ test/backup_test.go
@@ -3,7 +3,7 @@ package test
 import (
 	"fmt"
 	"strings"
-  "testing"
+	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
@@ -12,32 +12,32 @@ import (
 
 // Test the example in examples/one-db using Terratest.
 func TestBackupModule(t *testing.T) {
-  // Random generate a string for naming resources
-  uniqueID := strings.ToLower(random.UniqueId())
-  resourceName := fmt.Sprintf("test%s", uniqueID)
+	// Random generate a string for naming resources
+	uniqueID := strings.ToLower(random.UniqueId())
+	resourceName := fmt.Sprintf("test%s", uniqueID)
 
-  terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-    // Path to where our Terraform code is
-    TerraformDir: "../examples/vault",
-    Upgrade:      true,
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// Path to where our Terraform code is
+		TerraformDir: "../examples/vault",
+		Upgrade:      true,
 
-    // Variables to pass using -var-file option
-    Vars: map[string]interface{} {
-      "name_prefix": resourceName,
-    },
-  })
+		// Variables to pass using -var-file option
+		Vars: map[string]interface{}{
+			"name_prefix": resourceName,
+		},
+	})
 
-  // At the end of the test, run `terraform destroy` to clean up any resources that were created
-  defer terraform.Destroy(t, terraformOptions)
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptions)
 
-  // Run `terraform init` and `terraform apply` and fail the test if there are any errors
-  terraform.InitAndApply(t, terraformOptions)
+	// Run `terraform init` and `terraform apply` and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptions)
 
-  // Run `terraform output` to get the values of output variables
-  backupPlanArn := terraform.Output(t, terraformOptions, "backup_plan_arn")
-  backupVaultID := terraform.Output(t, terraformOptions, "backup_vault_id")
+	// Run `terraform output` to get the values of output variables
+	backupPlanArn := terraform.Output(t, terraformOptions, "backup_plan_arn")
+	backupVaultID := terraform.Output(t, terraformOptions, "backup_vault_id")
 
-  // Verify we're getting back the outputs we expect
-  assert.Contains(t, backupPlanArn, "arn:aws:backup:eu-west-1:")
-  assert.Contains(t, backupVaultID, "vault")
+	// Verify we're getting back the outputs we expect
+	assert.Contains(t, backupPlanArn, "arn:aws:backup:eu-west-1:")
+	assert.Contains(t, backupVaultID, "vault")
 }
