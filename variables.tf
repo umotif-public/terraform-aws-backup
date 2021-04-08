0d049821c4276d2d186015ce0b481ebc70e8c460
Abdul Wahid <wahid.abdul.m@gmail.com>
2021-04-08 17:15:12 +0100

    Revert "Cleanup (#7)"
    This reverts commit d0b5baf9d5ee3985688c021ddadadb82caa56d92.

--- variables.tf
+++ variables.tf
@@ -1,7 +1,56 @@
+variable "iam_role_name" {
+  description = "Name of IAM Role to associate to the Backup Plan"
+  type        = string
+  default     = null
+}
 
-##################
+# Rules
+variable "rules" {
+  description = "A list of rules mapping rule configurations for a backup plan"
+  type        = any
+  default     = []
+}
+
+# Selection
+variable "selection_name" {
+  description = "The display name of a resource selection document"
+  type        = string
+  default     = null
+}
+
+variable "selection_resources" {
+  description = "A list of strings that either contain Amazon Resource Names (ARNs) or match patterns of resources to assign to a backup plan"
+  type        = list(string)
+  default     = []
+}
+
+variable "selection_tag_type" {
+  description = "An operation, such as StringEquals, that is applied to a key-value pair used to filter resources in a selection"
+  type        = string
+  default     = null
+}
+
+variable "selection_tag_key" {
+  description = "The key in a key-value pair"
+  type        = string
+  default     = null
+}
+
+variable "selection_tag_value" {
+  description = "The value in a key-value pair"
+  type        = string
+  default     = null
+}
+
+variable "selection_tags" {
+  description = "A list of selection tags map"
+  type        = list(any)
+  default     = []
+}
+
+#
 # AWS Backup vault
-##################
+#
 variable "vault_name" {
   description = "Name of the backup vault to create. If not given, AWS use default"
   type        = string
@@ -20,47 +69,72 @@ variable "tags" {
   default     = {}
 }
 
-#################
+#
 # AWS Backup plan
-#################
+#
 variable "plan_name" {
   description = "The display name of a backup plan"
   type        = string
 }
 
-variable "rules" {
-  description = "A list of rules mapping rule configurations for a backup plan"
-  type        = any
-  default     = []
+variable "rule_name" {
+  description = "An display name for a backup rule"
+  type        = string
+  default     = null
+}
+
+variable "rule_schedule" {
+  description = "A CRON expression specifying when AWS Backup initiates a backup job"
+  type        = string
+  default     = null
+}
+
+variable "rule_start_window" {
+  description = "The amount of time in minutes before beginning a backup"
+  type        = number
+  default     = null
+}
+
+variable "rule_completion_window" {
+  description = "The amount of time AWS Backup attempts a backup before canceling the job and returning an error"
+  type        = number
+  default     = null
+}
+
+variable "rule_recovery_point_tags" {
+  description = "Metadata that you can assign to help organize the resources that you create"
+  type        = map(string)
+  default     = {}
+}
+
+# Rule lifecycle
+variable "rule_lifecycle_cold_storage_after" {
+  description = "Specifies the number of days after creation that a recovery point is moved to cold storage"
+  type        = number
+  default     = null
+}
+
+variable "rule_lifecycle_delete_after" {
+  description = "Specifies the number of days after creation that a recovery point is deleted. Must be 90 days greater than `cold_storage_after`"
+  type        = number
+  default     = null
+}
+
+# Rule copy action
+variable "rule_copy_action_lifecycle" {
+  description = "The lifecycle defines when a protected resource is copied over to a backup vault and when it expires."
+  type        = map(any)
+  default     = {}
+}
+
+variable "rule_copy_action_destination_vault_arn" {
+  description = "An Amazon Resource Name (ARN) that uniquely identifies the destination backup vault for the copied backup."
+  type        = string
+  default     = null
 }
 
 variable "advanced_backup_settings" {
   description = "An object that specifies backup options for each resource type"
   type        = any
   default     = []
-}
-
-# Selection
-variable "selection_name" {
-  description = "The display name of a resource selection document"
-  type        = string
-  default     = null
-}
-
-variable "selection_resources" {
-  description = "A list of strings that either contain Amazon Resource Names (ARNs) or match patterns of resources to assign to a backup plan"
-  type        = list(string)
-  default     = []
-}
-
-variable "selection_tags" {
-  description = "A list of selection tags map"
-  type        = list(any)
-  default     = []
-}
-
-variable "iam_role_name" {
-  description = "Name of IAM Role to associate to the Backup Plan"
-  type        = string
-  default     = null
 }
